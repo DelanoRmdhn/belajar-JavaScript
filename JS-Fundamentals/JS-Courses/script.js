86776cec754eb68895966ff801558c4da952e0a1
Delano <del.191006@gmail.com>
2023-10-21 11:30:11 +0700

    Dynamic Typing & typeof

--- JS-Fundamentals/JS-Courses/script.js
+++ JS-Fundamentals/JS-Courses/script.js
@@ -1,3 +1,16 @@
 // VARIABEL DAN NILAI
 let userName = "Delano";
 console.log(userName);
+
+// DYNAMIC TYPING DAN TYPEOF
+let age = 30;
+console.log(typeof age);
+
+//contoh dynamic Typing
+let firstName = "Delano";
+console.log(firstName);
+
+firstName = "ilham";
+console.log(firstName);
+
+//kesimpulannya dynamic typing memungkinkan suatu variabel mampu menimpa nilai yang berbeda
