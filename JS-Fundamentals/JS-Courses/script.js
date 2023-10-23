c833add45c2a83b2dd2402e1513a58b4a85388cc
Delano <del.191006@gmail.com>
2023-10-23 10:26:04 +0700

    If Else Statement

--- JS-Fundamentals/JS-Courses/script.js
+++ JS-Fundamentals/JS-Courses/script.js
@@ -12,5 +12,25 @@ console.log(firstName);
 
 firstName = "ilham";
 console.log(firstName);
-
 //kesimpulannya dynamic typing memungkinkan suatu variabel mampu menimpa nilai yang berbeda
+
+//IF ELSE STATEMENT
+const tahunSekarang = 2023;
+const tahunKelahiran = 2010;
+const umurMinimal = 17;
+
+if (tahunSekarang - tahunKelahiran >= umurMinimal) {
+  console.log(
+    `Umur anda saat ini adalah ${
+      tahunSekarang - tahunKelahiran
+    }. Maka anda boleh membuat SIM`
+  );
+} else {
+  console.log(
+    `Umur anda saat ini adalah ${
+      tahunSekarang - tahunKelahiran
+    }. Anda baru boleh membuat SIM ${
+      umurMinimal - (tahunSekarang - tahunKelahiran)
+    } tahun lagi.`
+  );
+}
