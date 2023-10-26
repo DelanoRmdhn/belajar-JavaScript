c8bce1e4b6d2ea2630b3d7e707b46d6b1a12585d
Delano <del.191006@gmail.com>
2023-10-26 09:49:58 +0700

    Fixing Aritmethic Counter in Challanges 3

--- JS-Fundamentals/JS-Challanges/script.js
+++ JS-Fundamentals/JS-Challanges/script.js
@@ -26,6 +26,7 @@ const bill2 = 40;
 const bill3 = 430;
 
 // BILL 1
+console.log("BILL 1 :");
 tip = bill1 >= 50 && bill1 <= 300 ? bill1 * 0.15 : bill1 * 0.2;
 let total1 = bill1 + tip;
 
@@ -33,13 +34,15 @@ console.log(`The bill was ${bill1}, the tip was ${tip}, and the total value
 is ${total1}`);
 
 //BILL 2
+console.log("BILL 2 :");
 tip = bill2 >= 50 && bill2 <= 300 ? bill2 * 0.15 : bill2 * 0.2;
-let total2 = bill1 + tip;
+let total2 = bill2 + tip;
 
 console.log(`The bill was ${bill2}, the tip was ${tip}, and the total value
 is ${total2}`);
 
 //BILL 3
+console.log("BILL 3 :");
 tip = bill3 >= 50 && bill3 <= 300 ? bill3 * 0.15 : bill3 * 0.2;
 let total3 = bill1 + tip;
 
