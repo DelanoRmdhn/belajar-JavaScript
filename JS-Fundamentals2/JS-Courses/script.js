5a72d80d0968cbefa623fc25c3b3d46a96226e2e
Delano <del.191006@gmail.com>
2023-11-01 21:31:54 +0700

    Looping in arrray (showing propperty of an array & inserting property of an array to a new array) & break continue

--- JS-Fundamentals2/JS-Courses/script.js
+++ JS-Fundamentals2/JS-Courses/script.js
@@ -5,3 +5,45 @@ let x = 1;
 for (x; x <= 10; x++) {
   console.log(`Perulangan ke- ${x}`);
 }
+
+const arrayDelano = [
+  "Delano",
+  "Ramadhan",
+  2023 - 2006,
+  "Web Developer",
+  ["ilham", "kurniawan", "edi"],
+];
+
+const arrayKosongan = [];
+
+for (let i = 0; i <= arrayDelano.length; i++) {
+  //menampilkan properti data arrayDelano
+  console.log(arrayDelano[i]);
+
+  //menambahkan properti data arrayKosongan dari properti data arrayDelano
+  arrayKosongan.push(arrayDelano[i]);
+}
+
+const tahunLahir = [1976, 1981, 2006, 2009, 2017];
+const umur = [];
+
+for (let i = 0; i < tahunLahir.length; i++) {
+  const hitungUmur = 2023 - tahunLahir[i];
+
+  umur.push(hitungUmur);
+}
+console.log(umur);
+
+//BREAK DAN CONTINUE
+
+//continue
+for (let i = 0; i <= arrayDelano.length; i++) {
+  if (typeof arrayDelano[i] !== "number") continue;
+  console.log(arrayDelano[i]);
+}
+
+//break
+for (let i = 0; i <= arrayDelano.length; i++) {
+  if (typeof arrayDelano[i] === "number") break;
+  console.log(arrayDelano[i]);
+}
