3789f01b53b72cb9f242ec4b3a6b6d16de1eba38
Delano <del.191006@gmail.com>
2023-11-02 14:23:52 +0700

    Coding Challanges6 Finished (Looping in array & Nested Looping in array)

--- JS-Fundamentals2/JS-Challanges/script.js
+++ JS-Fundamentals2/JS-Challanges/script.js
@@ -1,51 +1,64 @@
 /* 
-Back to the two gymnastics teams, the Dolphins and the Koalas! There is a new
-gymnastics discipline, which works differently.
-Each team competes 3 times, and then the average of the 3 scores is calculated (so
-one average score per team).
-
-A team only wins if it has at least double the average score of the other team.
-Otherwise, no team wins!
-
+Let's improve Steven's tip calculator even more, this time using loops!
 Your tasks:
-1. Create an arrow function 'calcAverage' to calculate the average of 3 scores
-2. Use the function to calculate the average for both teams
-3. Create a function 'checkWinner' that takes the average score of each team
-as parameters ('avgDolhins' and 'avgKoalas'), and then logs the winner
-to the console, together with the victory points, according to the rule above.
-Example: "Koalas win (30 vs. 13)"
-4. Use the 'checkWinner' function to determine the winner for both Data 1 and
-Data 2
-5. Ignore draws this time
 
-Test data:
-§ Data 1: Dolphins score 44, 23 and 71. Koalas score 65, 54 and 49
-§ Data 2: Dolphins score 85, 54 and 41. Koalas score 23, 34 and 27
+1. Create an array 'bills' containing all 10 test bill values
+
+2. Create empty arrays for the tips and the totals ('tips' and 'totals')
+
+3. Use the 'calcTip' function we wrote before (no need to repeat) to calculate
+tips and total values (bill + tip) for every bill value in the bills array. Use a for
+loop to perform the 10 calculations!
+Test data: 22, 295, 176, 440, 37, 105, 10, 1100, 86 and 52
+Hints: Call ‘calcTip ‘in the loop and use the push method to add values to the
+tips and totals arrays �
+Bonus:
+
+4. Bonus: Write a function 'calcAverage' which takes an array called 'arr' as
+an argument. This function calculates the average of all numbers in the given
+array. This is a difficult challenge (we haven't done this before)! Here is how to
+solve it:
+
+4.1. First, you will need to add up all values in the array. To do the addition,
+start by creating a variable 'sum' that starts at 0. Then loop over the
+array using a for loop. In each iteration, add the current value to the
+'sum' variable. This way, by the end of the loop, you have all values
+added together
+
+4.2. To calculate the average, divide the sum you calculated before by the
+length of the array (because that's the number of elements)
+
+4.3. Call the function with the 'totals' array
 */
 
-// Calculating average each teams
-const calcAverage = (score1, score2, score3) => (score1 + score2 + score3) / 3;
+const bills = [2, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+const tips = [];
+const totals = [];
 
-// DATA 1
-let scoreDolphins = calcAverage(44, 23, 71);
-let scoreKoalas = calcAverage(65, 54, 49);
+const calcTip = function (bill) {
+  return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+};
 
-console.log(scoreDolphins, scoreKoalas);
+let test = 1;
+for (let i = 0; i <= bills.length - 1; i++) {
+  const tip = calcTip(bills[i]);
+  tips.push(tip);
 
-// Checking Winner
-function checkWinner(avgDolhins, avgKoalas) {
-  if (avgDolhins >= 2 * avgKoalas) {
-    console.log(`Dolphins win (${avgDolhins} vs. ${avgKoalas})`);
-  } else if (avgKoalas >= 2 * avgDolhins) {
-    console.log(`Koalas win (${avgKoalas} vs. ${avgDolhins})`);
-  } else {
-    console.log("No team wins");
-  }
+  totals.push(tip + bills[i]);
+  console.log(
+    `Total Bill Test ${test}: Bill : ${bills[i]} Tip : ${tip}. Total : ${totals[i]}`
+  );
+  test++;
 }
 
-checkWinner(scoreDolphins, scoreKoalas);
+// BONUS
+calcAverage = function (arr) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+    sum += arr[i];
+  }
+  return sum / arr.length;
+};
 
-//DATA 2
-scoreDolphins = calcAverage(85, 54, 41);
-scoreKoalas = calcAverage(23, 34, 27);
-checkWinner(scoreDolphins, scoreKoalas);
+console.log(calcAverage(bills));
