aa68fd2b8f4fbb1152a435a7653de2fa8ee119f8
Delano <del.191006@gmail.com>
2023-11-03 11:01:08 +0700

    DOM Manipulation (AddEventListener: Logic check 'Guess my Number')

--- JS-DOMEventsFundamentals/JS-Courses/05-Guess-My-Number/starter/script.js
+++ JS-DOMEventsFundamentals/JS-Courses/05-Guess-My-Number/starter/script.js
@@ -1,1 +1,15 @@
 'use strict';
+
+// DOM Manipulation untuk check
+const check = document.querySelector('.check');
+const guess = document.querySelector('.guess');
+const message = document.querySelector('.message');
+
+check.addEventListener('click', function () {
+  const guessValue = Number(guess.value);
+  console.log(guessValue);
+
+  if (!guessValue) {
+    message.textContent = '⛔ No number inputted!';
+  }
+});
