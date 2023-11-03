1a197c7d6d7b75b324e90755201083cfb5b55051
Delano <del.191006@gmail.com>
2023-11-03 11:41:33 +0700

    DOM Manipulation (Implementing Logic for the game: If Else statement,nested if else statement)

--- JS-DOMEventsFundamentals/JS-Courses/05-Guess-My-Number/starter/script.js
+++ JS-DOMEventsFundamentals/JS-Courses/05-Guess-My-Number/starter/script.js
@@ -4,12 +4,43 @@
 const check = document.querySelector('.check');
 const guess = document.querySelector('.guess');
 const message = document.querySelector('.message');
+const number = document.querySelector('.number');
+const score = document.querySelector('.score');
+
+//simpan data diluar DOM
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+number.textContent = secretNumber;
+
+let userScore = 20;
 
 check.addEventListener('click', function () {
   const guessValue = Number(guess.value);
   console.log(guessValue);
 
+  //kondisi user tidak menginput angka
   if (!guessValue) {
     message.textContent = '⛔ No number inputted!';
+  }
+  //kondisi ketika user benar
+  else if (guessValue === secretNumber) {
+    message.textContent = '🎉 Correct Number!';
+  } else if (guessValue > secretNumber) {
+    if (userScore > 1) {
+      message.textContent = '📈 To high!';
+      userScore--;
+      score.textContent = userScore;
+    } else {
+      message.textContent = '💥 You lost the game!';
+      score.textContent = 0;
+    }
+  } else if (guessValue < secretNumber) {
+    if (userScore > 1) {
+      message.textContent = '📈 To low!';
+      userScore--;
+      score.textContent = userScore;
+    } else {
+      message.textContent = '💥 You lost the game!';
+      score.textContent = 0;
+    }
   }
 });
