700d3473d16b1c0b6bfe2481562d2a51d663836f
Delano <del.191006@gmail.com>
2023-11-03 15:04:11 +0700

    PROJECT #1 FINISHED (guess my Number : Refactoring the code(Making the code easier to read and Implementing DRY))

--- JS-DOMEventsFundamentals/JS-Courses/05-Guess-My-Number/starter/script.js
+++ JS-DOMEventsFundamentals/JS-Courses/05-Guess-My-Number/starter/script.js
@@ -15,43 +15,42 @@ number.textContent = secretNumber;
 let userScore = 20;
 let userHighScore = 0;
 
+function displayMassage(messages) {
+  message.textContent = messages;
+}
+
 check.addEventListener('click', function () {
   const guessValue = Number(guess.value);
   console.log(guessValue);
 
   //kondisi user tidak menginput angka
   if (!guessValue) {
-    message.textContent = '⛔ No number inputted!';
+    displayMassage('⛔ No number inputted!');
   }
   //kondisi ketika user benar
   else if (guessValue === secretNumber) {
-    message.textContent = '🎉 Correct Number!';
+    displayMassage('🎉 Correct Number!');
     if (userScore > userHighScore) {
       userHighScore = userScore;
       highScore.textContent = userHighScore;
     }
-
     //styling
     body.style.backgroundColor = '#60b347';
     number.style.width = '30rem';
-  } else if (guessValue > secretNumber) {
+  } else if (guessValue !== secretNumber) {
     if (userScore > 1) {
-      message.textContent = '📈 To high!';
+      displayMassage(
+        guessValue > secretNumber ? '📈 Too high!' : '📉 Too low!'
+      );
       userScore--;
-      score.textContent = userScore;
+      score.textContent = 0;
     } else {
-      message.textContent = '💥 You lost the game!';
+      displayMassage('💥 You lost the game!');
       score.textContent = 0;
     }
-  } else if (guessValue < secretNumber) {
-    if (userScore > 1) {
-      message.textContent = '📈 To low!';
-      userScore--;
-      score.textContent = userScore;
-    } else {
-      message.textContent = '💥 You lost the game!';
-      score.textContent = 0;
-    }
+  } else {
+    displayMassage('💥 You lost the game!');
+    score.textContent = 0;
   }
 });
 
@@ -60,7 +59,7 @@ const again = document.querySelector('.again');
 // handler restart game
 again.addEventListener('click', function () {
   //reset message
-  message.textContent = 'Start guessing...';
+  displayMassage('Start guessing...');
   //reset randomNumber
   secretNumber = Math.trunc(Math.random() * 20) + 1;
   console.log(secretNumber);
