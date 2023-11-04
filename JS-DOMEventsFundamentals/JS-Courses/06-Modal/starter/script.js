adf8bab2c4d3d5765b17e1097a45b3f5375f5d67
Delano <del.191006@gmail.com>
2023-11-04 13:38:39 +0700

    Project Modal Window (Making Click Event, Changing addEventListener Property(Function using external Function))

--- JS-DOMEventsFundamentals/JS-Courses/06-Modal/starter/script.js
+++ JS-DOMEventsFundamentals/JS-Courses/06-Modal/starter/script.js
@@ -6,8 +6,20 @@ const openModalBtn = document.querySelectorAll('.show-modal');
 const closeModalBtn = document.querySelector('.close-modal');
 const overlay = document.querySelector('.overlay');
 
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
 for (let i = 0; i < openModalBtn.length; i++) {
-  openModalBtn[i].addEventListener('click', function () {
-    console.log('adakah perubahan?');
-  });
+  openModalBtn[i].addEventListener('click', openModal);
 }
+
+// CLOSE MODAL EVENT
+closeModalBtn.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
