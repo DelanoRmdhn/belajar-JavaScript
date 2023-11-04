2327f4f2000baf4bb7d18c1fd78d12142aced6ea
Delano <del.191006@gmail.com>
2023-11-04 10:59:30 +0700

    Project Modal WIndow (Declaring variable,introduction to querySelectorAll & acsessing querySelectorAll Classes)

--- JS-DOMEventsFundamentals/JS-Courses/06-Modal/starter/script.js
+++ JS-DOMEventsFundamentals/JS-Courses/06-Modal/starter/script.js
@@ -1,1 +1,13 @@
 'use strict';
+
+// DEKLARASI VARIABEL CLASS
+const modal = document.querySelector('.modal');
+const openModalBtn = document.querySelectorAll('.show-modal');
+const closeModalBtn = document.querySelector('.close-modal');
+const overlay = document.querySelector('.overlay');
+
+for (let i = 0; i < openModalBtn.length; i++) {
+  openModalBtn[i].addEventListener('click', function () {
+    console.log('adakah perubahan?');
+  });
+}
